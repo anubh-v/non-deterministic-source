52f21ce8d5924eeb219e80856aecad0635635be8
Arsalan Cheema <arsalanc.v2@gmail.com>
2020-03-01 20:05:52 +0800

    move MD puzzle to example file

--- test.js
+++ test.js
@@ -67,37 +67,6 @@ function test_distinct() {
 /* 1.2 Test Non-Deterministic Functionality */
 
 /**
- * Multiple Dwelling Puzzle
- * Taken from SICP JS section 4.3.2
-*/
-function test_nondet_multiple_dwelling() {
-    parse_and_eval("\
-        function multiple_dwelling() {\
-            const baker = amb(1, 2, 3, 4, 5);\
-            const cooper = amb(1, 2, 3, 4, 5);\
-            const fletcher = amb(1, 2, 3, 4, 5);\
-            const miller = amb(1, 2, 3, 4, 5);\
-            const smith = amb(1, 2, 3, 4, 5);\
-            require(distinct(list(baker, cooper, fletcher, miller, smith)));\
-            require(baker !== 5);\
-            require(cooper !== 1);\
-            require(fletcher !== 5);\
-            require(fletcher !== 1);\
-            require(miller > cooper);\
-            require(math_abs(smith - fletcher) === 1);\
-            require(math_abs(fletcher - cooper) === 1);\
-            return list(list('baker', baker),\
-                        list('cooper', cooper),\
-                        list('fletcher', fletcher),\
-                        list('miller', miller),\
-                        list('smith', smith));\
-        }\
-        const result = multiple_dwelling();\
-        result;\
-    ");   
-}
-
-/**
  * Tests the behaviour of the empty amb statement.
 */
 function test_nondet_empty() {
